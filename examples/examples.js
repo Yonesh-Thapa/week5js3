188c0106aef490cb0106b5c571931c962a715e8e
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-08 14:33:00 -0400

    first commit

---
+++ examples/examples.js
@@ -0,0 +1,14 @@
+let emp ={
+    0:"zero",
+    employeeId: "1",
+    name: "Ezra",
+    jobTitle: "Theater Teacher",
+    payRate: 38.46
+};
+console.log(emp.employeeId);// prints the specific property in the object
+console.log(emp); // prints the whole object
+console.log(emp["0"]); // prints undefined question
+console.log(emp["employeeId"]);// another way of printing specific property of an boject
+console.log(emp["name"]);
+
+
